6d2118d39316a12dd4d7a4acf8fef74c711325bd
Redemter <redemterrita@gmail.com>
2023-03-08 00:21:40 +0300

    gender update

--- public/JS/main.js
+++ public/JS/main.js
@@ -3,6 +3,7 @@ const WEEK_DAYS=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday
 const MALE_NAMES=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 const FEMALE_NAMES=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let akanName = null;
+let gender = null;
 
 
 function userInfo(){
@@ -11,7 +12,7 @@ function userInfo(){
     let year = parseInt(yearOfBirth.slice(2, ));
     let monthOfBirth = parseInt(prompt("Enter your month of birth:"));
     let dateOfBirth = parseInt(prompt("Enter the date of birth:"));
-    let gender = prompt("Enter your gender: ");
+    let gender = prompt("Enter your gender: f or m ");
     let dayOfTheWeek = Math.floor( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(monthOfBirth+1)/10)) + dateOfBirth ) % 7;
     return dayOfTheWeek;
     
